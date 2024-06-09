861e78a8e2189b9d80439291e9f0a53c3bb96461
JakubWielcaw <jwieclaw000@gmail.com>
2024-06-09 19:11:24 +0200

    add compile line

--- compile.sh
+++ compile.sh
@@ -1,2 +1,5 @@
 g++ -Iinclude -std=c++17 -O3 main.cpp solver/hamming.cpp -o hamming.exe
 g++ -Iinclude -fopenmp -std=c++17 -O3 main_parallel.cpp solver/hamming.cpp -o hamming_parallel.exe
+
+cd cuda
+nvcc -o main_cuda main_cuda.cpp hamming_cuda.cu ../solver/hamming.cpp
